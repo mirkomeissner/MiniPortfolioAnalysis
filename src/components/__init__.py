c9371b33d535a0c37cc360ad62dc36dd86c660f0
mirkomeissner <mirko.meissner@web.de>
2026-04-11 08:13:32 +0200

    Update __init__.py

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,4 +1,4 @@
 # In src/components/__init__.py
-from .asset_management import asset_table_view, asset_bulk_form
+from .asset_management import asset_table_view
 from .transaction_management import transaction_table_view, transaction_bulk_form
 from .ticker_search import ticker_search_view
