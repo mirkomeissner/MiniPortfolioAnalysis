df18b1ddda19454fc382f1b3cd542bd0c28ca49a
mirkomeissner <mirko.meissner@web.de>
2026-04-25 22:31:34 +0000

    accounts management

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -3,10 +3,12 @@ from .asset_management import asset_table_view
 from .transaction_management import transaction_table_view
 from .ticker_search import ticker_search_view
 from .admin_management import admin_approval_page
+from .accounts_management import accounts_settings_view
 
 __all__ = [
     'asset_table_view',
     'transaction_table_view',
     'ticker_search_view',
-    'admin_approval_page'
+    'admin_approval_page',
+    'accounts_settings_view'
 ]
